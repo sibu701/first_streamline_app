c9a8391bfd508da907a43b88c910486ffb8cc366
sibu701 <110457130+sibu701@users.noreply.github.com>
2023-11-14 18:02:45 +0500

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,8 +1,4 @@
 import streamlit
-import pandas
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My First APP')
 streamlit.header('Breakfast Menu')
@@ -10,15 +6,18 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
+import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 
 
-streamlit.dataframe(my_fruit_list)
+
 
 
 
